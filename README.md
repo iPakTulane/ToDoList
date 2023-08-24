44e19e39e86f7963fdce193345dd34986ca6216f
iPakTulane <ipak.tulane@gmail.com>
2023-08-24 15:40:21 +0500

    Add readme and gitignore

---
+++ README.md
@@ -0,0 +1,11 @@
+#Swift - To Do List App
+
+This is a simple to do list app in swift showcasing: 
+
+- add, view, and interact with tasks 
+- view controllers 
+- models 
+- views 
+- storyboards
+- passing data between controllers
+- saving data
--- ToDoListSwift/ViewController.swift
+++ ToDoListSwift/ViewController.swift
@@ -9,11 +9,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
 }
 
+extension ViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+}
+
+extension ViewController: UITableViewDataSource {
+    
+}
